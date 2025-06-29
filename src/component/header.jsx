7bb5d22f9587e65c8464a46748c77fb931c1b7ea
suresh32332 <sureshsuresh32332@gmail.com>
2025-06-29 19:36:43 +0530

    create project  react+vite + tailwindcss and header

---
+++ src/component/header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+       <header class="flex justify-between px-4">
+            <a href="#">Suresh</a>
+            <ul class="flex">
+              <li><a href="#" >About me</a></li>
+              <li><a href="#" >Projects</a></li>
+              <li><a href="#" >Resume</a></li>
+              <li><a href="#" >Contact</a></li>
+            </ul>
+       </header>
+      
+    )
+}
