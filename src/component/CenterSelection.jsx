79b6d734275f88ce74a0adf489999c996393d8cc
suresh32332 <sureshsuresh32332@gmail.com>
2025-07-02 23:28:47 +0530

    add the fonts and create the icons

--- src/component/CenterSelection.jsx
+++ src/component/CenterSelection.jsx
@@ -1,12 +1,24 @@
 import dev from "../assets/dev.jpg";
+import { CiLinkedin } from "react-icons/ci";
+import { CiTwitter } from "react-icons/ci";
+import { CiInstagram } from "react-icons/ci";
+export default function CenterSelection() {
 
-export default function CenterSelection(){
+  return (
+    <section className="flex px-5 py-32 bg-secondary justify-center">
+      <div className=" w-1/2 flex flex-col font-poppins">
+        <h1 className="text-white text-5xl ">Hey, I am Suresh
+          <p className="text-2xl py-2">  Java full Stack developer </p></h1>
+        
 
-    return(
-      <section className="flex px-20 py-30 bg-secondary">
-        <h1 className="text-white w-1/3 text-3xl">Hey, I am Suresh 
-           <p>  Java full Stack developer </p></h1>
-            <img className=" w-1/3" src={dev} />
-      </section>
-    )
+        <div className=" flex   py-10">
+          <a href="#" className="pr-5 hover:text-white"><CiLinkedin size={40} /></a>
+          <a href="#" className="pr-5 hover:text-white"><CiInstagram size={40}/></a>
+        <a href="#" className="pr-5 hover:text-white"><CiTwitter size={40}/></a>
+        </div>
+
+      </div>
+      <img className=" w-1/3" src={dev} />
+    </section>
+  )
 }
